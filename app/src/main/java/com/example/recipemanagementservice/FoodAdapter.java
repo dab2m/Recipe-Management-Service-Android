f728191679b30dcc3903df0cda2cf3dfe3da5b38
mustafatozluoglu <m_saidt@hotmail.com>
2019-07-06 17:55:29 +0300

    Added delete button for each recipe

--- app/src/main/java/com/example/recipemanagementservice/FoodAdapter.java
+++ app/src/main/java/com/example/recipemanagementservice/FoodAdapter.java
@@ -6,8 +6,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,23 +21,29 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.MyViewHolder> 
     ArrayList<Food> mFoodList;
     LayoutInflater inflater;
 
-    public FoodAdapter(Context context, ArrayList<Food> foods){
-        inflater=LayoutInflater.from(context);
-        this.mFoodList=foods;
+    public FoodAdapter(Context context, ArrayList<Food> foods) {
+        inflater = LayoutInflater.from(context);
+        this.mFoodList = foods;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view=inflater.inflate(R.layout.item_recipe, viewGroup, false);
-        MyViewHolder holder=new MyViewHolder(view);
+        View view = inflater.inflate(R.layout.item_recipe, viewGroup, false);
+        MyViewHolder holder = new MyViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Food selectFood=mFoodList.get(position);
+        Food selectFood = mFoodList.get(position);
         holder.setData(selectFood, position);
+        holder.bDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("ASDASd");
+            }
+        });
     }
 
     @Override
@@ -43,19 +51,21 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.MyViewHolder> 
         return mFoodList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView foodName, foodDescription, foodTag;
         ImageView foodImage;
+        Button bDelete;
 
-        public MyViewHolder(View itemView){
+        public MyViewHolder(View itemView) {
             super(itemView);
-            foodName=(TextView)itemView.findViewById(R.id.yemekIsmi);
-            foodDescription=(TextView)itemView.findViewById(R.id.yemekAciklamasi);
-            foodTag=(TextView)itemView.findViewById(R.id.yemekEtiketleri);
-            foodImage=(ImageView)itemView.findViewById(R.id.yemekResmi);
+            foodName = (TextView) itemView.findViewById(R.id.yemekIsmi);
+            foodDescription = (TextView) itemView.findViewById(R.id.yemekAciklamasi);
+            foodTag = (TextView) itemView.findViewById(R.id.yemekEtiketleri);
+            foodImage = (ImageView) itemView.findViewById(R.id.yemekResmi);
+            bDelete = (Button) itemView.findViewById(R.id.bDelete);
         }
 
-        public void setData(Food selectedFood,int position){
+        public void setData(Food selectedFood, int position) {
             this.foodName.setText(selectedFood.getFoodName());
             this.foodDescription.setText(selectedFood.getFoodDescription());
             this.foodTag.setText(selectedFood.getFoodTag());
